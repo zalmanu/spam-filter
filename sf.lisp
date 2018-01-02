963ef603cd39e3f22bba11836285b16675532e83
Emanuel Danci <danci.emanuel@gmail.com>
2018-01-02 19:20:29 +0200

    Add Laplace smoothing + small refactorings

--- sf.lisp
+++ sf.lisp
@@ -18,16 +18,24 @@
 (defparameter *total-ham-words* 0)
 (defparameter *total-spams* 0)
 (defparameter *total-hams* 0)
+(defparameter *total-words* 0)
+(defparameter *alpha* 1)
 
 (defun split (text)
   (delete-duplicates
     (cl-ppcre:all-matches-as-strings "[a-zA-Z]{2,}" text)
     :test #'string=))
 
-(defun save-word (word) 
+(defun get-or-create-word (word)
   (or (gethash word *words-db*) 
       (setf (gethash word *words-db*) 
             (make-instance 'words-count))))
+
+(defun save-word (word) 
+ (get-or-create-word word))
+
+(defun get-word(word) 
+ (get-or-create-word word))
 
 (defun extract-words (text) 
   (mapcar #'save-word (split text)))
@@ -50,7 +58,8 @@
 (defun increment-total-words-count (type)
  (ecase type
   (ham (incf *total-ham-words*))
-  (spam (incf *total-spam-words*))))
+  (spam (incf *total-spam-words*)))
+ (incf *total-words*))
 
 (defun increment-total-count (type) 
  (ecase type 
@@ -65,10 +74,10 @@
   (increment-total-count type))
 
 (defun get-word-prob (word type)
- (with-slots (ham-cnt spam-cnt) (gethash word *words-db*)
+ (with-slots (ham-cnt spam-cnt) (get-word word)
   (ecase type
-   (ham (/ ham-cnt *total-ham-words*))
-   (spam (/ spam-cnt *total-spam-words*)))))
+   (ham (/ (+ ham-cnt *alpha*) (+ *total-ham-words* (* *alpha* *total-words*))))
+   (spam (/ (+ spam-cnt *alpha*) (+ *total-spam-words* (* *alpha* *total-words*)))))))
 
 (defun get-text-probab (text type)
  (let ((res 1.0))
@@ -81,4 +90,6 @@
  (setq ham-prob (/ *total-hams* (+ *total-spams* *total-hams*)))
  (setq is-spam-prob (get-text-probab text 'spam))
  (setq is-ham-prob (get-text-probab text 'ham))
- (> (* spam-prob is-spam-prob) (* ham-prob is-ham-prob)))
+ (if (> (* spam-prob is-spam-prob) (* ham-prob is-ham-prob))
+  (return-from classify 'spam)
+  (return-from classify 'ham)))
